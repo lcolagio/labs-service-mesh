93e7d9c61c9eb397e524577c30c590db7adac003
lcolagio <66462164+lcolagio@users.noreply.github.com>
2020-12-16 16:51:54 +0100

    Create 100-demo-user.sh

---
+++ application-demo/app1/100-demo-user.sh
@@ -0,0 +1,15 @@
+
+export MY_SM_SMCP=mgen-servicemesh-cp
+export MY_SM_APP=mgen-servicemesh-app-1
+export SUBDOMAIN=$(oc get ingresses.config.openshift.io cluster -o jsonpath='{.spec.domain}')
+export MY_SM_APP_ROUTE=$(oc get route ${MY_SM_APP} -n ${MY_SM_SMCP} -o 'jsonpath={.spec.host}')
+
+# from external route
+#
+while true; do curl ${MY_SM_ROUTE}; sleep 1; done
+while true; do curl ${MY_SM_ROUTE}/customer; sleep 1; done
+while true; do curl ${MY_SM_ROUTE}/recommendation; sleep 1; done
+
+# from curl pod
+#
+oc rsh $(oc get pods -l app=curl --no-headers | awk '{print $1}') curl preference:8080
