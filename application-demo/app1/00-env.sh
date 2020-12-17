75707071a64fa4a85ca38ae65f808e23903bf843
lcolagio <66462164+lcolagio@users.noreply.github.com>
2020-12-17 11:21:38 +0100

    Update 00-env.sh

--- application-demo/app1/00-env.sh
+++ application-demo/app1/00-env.sh
@@ -1,3 +1,8 @@
 export MY_SM_SMCP=mgen-servicemesh-cp
-export MY_SM_APP=mgen-servicemesh-app-1
+export MY_SM_APP=mgen-servicemesh-app1
 export SUBDOMAIN=$(oc get ingresses.config.openshift.io cluster -o jsonpath='{.spec.domain}')
+
+echo $MY_SM_SMCP
+echo $MY_SM_APP
+echo $SUBDOMAIN
+
